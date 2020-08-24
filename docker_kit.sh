cf13710b6907e44275c10e02a6f5e9822d47a2cb
killDevils <killdevils@icloud.com>
2020-08-24 14:31:34 +0800

    update

---
+++ docker_kit.sh
@@ -0,0 +1,66 @@
+# source <(curl -s https://raw.githubusercontent.com/killDevils/PublicSource/master/docker_kit.sh)
+
+
+rmc(){
+  if [ -z $1 ]; then
+    echo "Parameter missing.
+    to remove all containers: rmc all
+    to delete a certain one: rmc '$container_name'
+    to delete all stopped: rmc stopped"
+    return 0
+  fi
+  running_containers=$(docker container ls -q)
+  all_containers=$(docker container ls -aq)
+  if [ $1 == "all" ]; then
+    if [ -z "$all_containers" ]; then
+      echo "No container exists."
+      return 0
+    fi
+    if [ -n "$running_containers" ]; then
+      echo "Shutting down all running containers..."
+      docker stop $running_containers
+      echo "All containers stopped."
+    fi
+    echo "Removing: $all_containers"
+    docker rm $all_containers
+  elif [ $1 == "stopped" ]; then
+    docker rm $(docker ps -a -q) 2> /dev/null
+  else
+    docker rm $1
+  fi
+}
+
+
+lsc(){
+  if [ -z $1 ]; then
+    echo "缺参数。
+    罗列所有：“lsc all”。
+    罗列运行中的：“lsc running”。
+    罗列停止了的：“lsc stopped”。"
+    return 0
+  fi
+  case $1 in
+    all ) docker ps -a;;
+    running ) docker ps --filter status=running;;
+    stopped ) docker ps --filter status=exited;;
+  esac
+}
+
+rmi(){
+  if [ -z $1 ]; then
+    echo "缺参数。
+    删除所有：“rmi all”。
+    删除某一个：“rmi 镜像名”。"
+    return 0
+  fi
+  if [ $1 == "all" ]; then
+    docker rmi -f $(docker image ls -q) 2> /dev/null
+  else
+    docker rmi -f $1
+  fi
+}
+
+
+lsi(){
+  docker image ls
+}
---
+++ install_docker_and_compose.sh
@@ -0,0 +1,63 @@
+# bash <(curl -s https://raw.githubusercontent.com/killDevils/PublicSource/master/install_docker_and_compose.sh)
+
+OS_Debian=$(cat /etc/os-release | head -1 | grep Debian)
+OS_Ubuntu=$(cat /etc/os-release | head -1 | grep Ubuntu)
+
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+
+if [ -n "$OS_Debian" ]; then
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+elif [ -n "$OS_Ubuntu" ]; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository \
+     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     $(lsb_release -cs) \
+     stable"
+elif [ -n "$OS_Debian" ] && [ -n "$OS_Ubuntu" ]; then
+  echo "This script supports only Debian or Ubuntu. For other OSs, go to https://docs.docker.com/engine/install/ for help"
+fi
+
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
