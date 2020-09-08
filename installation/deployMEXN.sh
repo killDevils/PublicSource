f0205324a0c884d445ec82a5496a9d6fca832268
killDevils <killdevils@icloud.com>
2020-09-08 13:06:21 +0800

    Create deployMEXN.sh

---
+++ installation/deployMEXN.sh
@@ -0,0 +1,21 @@
+# this script is suitable for deploying following packages in ubuntu 20.04 LTS container:
+# curl
+# nginx
+# nodejs 14.x
+
+# Since in docker official container Ubuntu 20.04, default user is root, so, the following commands do not use sudo.
+apt update -y
+apt install curl wget nano -y
+
+apt install nginx -y
+
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+apt-get install -y nodejs
+
+# wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | apt-key add -
+#
+# echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+#
+# apt-get update
+#
+# apt-get install -y mongodb-org
